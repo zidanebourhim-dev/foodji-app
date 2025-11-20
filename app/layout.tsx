4de88d59ecb7d3316efadf375624a7ae1ef99cc3
zidanebourhim-dev <zidane.bourhim@gmail.com>
2025-11-20 18:26:30 +0100

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Foodji',
+  description: 'L\'éruption des saveurs à Sala Al Jadida',
+  manifest: '/manifest.json',
+}
+
+export const viewport = {
+  themeColor: '#151e32',
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="fr">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/foodji.png" />
+        <meta name="theme-color" content="#151e32" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      </head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
