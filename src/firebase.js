27c862b59e46f6aeac80784532e34fe4d9653424
null <null>
2026-01-05 00:24:31 +0100

    vf1

--- src/firebase.js
+++ src/firebase.js
@@ -11,8 +11,11 @@ const firebaseConfig = {
   appId: "1:332967788626:web:9eab671b117e2033969f8b"
 };
 
+// Initialisation
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
+// On exporte tout d'un coup pour éviter le bug d'initialisation
 export { db, auth };
+export default app;
