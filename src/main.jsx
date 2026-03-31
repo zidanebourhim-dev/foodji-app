c6fd85cd8dd397abd06ce329af2da0e14ab17726
zidanebourhim-dev <zidane.bourhim@gmail.com>
2026-03-31 18:09:38 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Script de destruction forcée des vieux Service Workers (Le Nettoyeur)
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (let registration of registrations) {
+      registration.unregister();
+      console.log("Ancien Service Worker fantôme abattu.");
+    }
+  });
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
